81d30477b3dc56285f679159d4f2b3d359c8b35a
somelameguy <aglin@ucsc.edu>
2014-11-21 15:25:57 -0800

    BHDS folder is the message one

---
+++ bhds/Assets/shoot.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shoot : MonoBehaviour {
+	public float speed = 0.1f;
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, 7f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+				
+	
+		transform.position += transform.up * Time.deltaTime * speed;
+	}
+	void OnMouseOver(){
+	
+						//GameObject Textbox = GameObject.Find ("Textbox");
+						//myGUI playerscript = Textbox.GetComponent<myGUI> ();
+						//playerscript.messages = "FUAHK";
+						//playerscript.response ();
+						Destroy (gameObject);
+				
+	}
+}
