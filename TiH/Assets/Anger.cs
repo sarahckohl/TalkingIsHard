9ac5961d9199a559eff331043feeb7a9af2bca58
Sarah Kohl <sarahcharlottekohl@gmail.com>
2014-11-23 23:47:14 -0800

    FEELS I coded some feels.  Anger, mainly. You can now get angry.  And see just how angry with an onscreen printout. RAWR.

---
+++ TiH/Assets/Anger.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Anger : MonoBehaviour {
+
+	GameController gameController;
+	float speed = 1.2f;
+
+
+	// Use this for initialization
+	void Start () {
+
+		gameController = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController>();
+		rigidbody2D.velocity = Vector3.down * speed;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other){
+		
+		
+		if(other.gameObject.tag ==("Bullet" )){
+			return;
+		}
+		if (other.gameObject.tag == ("Player")) {
+			gameController.AngerUp();
+			Destroy (gameObject);
+		}
+		
+		
+	}
+
+
+
+}
